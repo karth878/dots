adcfe7753dab35650bcbbb346fbc8e4443b9ee28
Venkat Karthik Nittala <nittalav@rider.edu>
2025-03-30 20:31:43 -0400

    Add files via upload

--- flake.nix
+++ flake.nix
@@ -4,12 +4,15 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     niri-flake.url = "github:sodiboo/niri-flake";
+    hyprpanel.url = "github:Jas-SinghFSU/HyprPanel";
   };
 
-  outputs = { self, nixpkgs, niri-flake, ... }@inputs: {
+  outputs = { self, nixpkgs, niri-flake, hyprpanel, ... }@inputs: {
     nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux"; # Change this if using a different architecture
-      specialArgs = { inherit inputs; };
+      specialArgs = {
+      inherit inputs;
+      };
       modules = [
         ./configuration.nix
         inputs.niri-flake.nixosModules.niri
@@ -19,6 +22,9 @@
 
           # Optional: set as default session for your display manager
           services.displayManager.defaultSession = "niri";
+          
+          # Add HyprPanel overlay
+          nixpkgs.overlays = [inputs.hyprpanel.overlay];
         }
       ];
     };
